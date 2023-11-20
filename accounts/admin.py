2267a01c6f4c230c655c79c88dbbd88858433b96
uya2511 <ktq2390320@stu.o-hara.ac.jp>
2023-11-20 10:30:06 +0900

    Commit

---
+++ accounts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import CustomUser
+
+class CustomUserAdomin(admin.ModelAdmin):
+
+
+    list_display = ('id', 'username')
+
+    list_display_links = ('id', 'username')
+
+admin.site.register(CustomUser, CustomUserAdomin)
